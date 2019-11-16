7d2f430f30d2d5452a0cf3f02a7920ea3b78ce1a
sai-kranthi-au5 <iamsaikranthi@gmail.com>
2019-11-17 00:09:10 +0530

    base pages created

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const hbs = exphbs.create({
-  extname: ".hbs"
+  extname: ".hbs",
+  helpers: {
+    ifCond: function(v1, v2, options) {
+      if (v1 === v2) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    }
+  }
 });
 
 app.engine(".hbs", hbs.engine);
@@ -21,16 +29,53 @@ app.set("view engine", ".hbs");
 
 app.get("/", function(request, response) {
   response.render("home", {
-    title: "Flash Rides - Flash speed commuting service."
+    title: "Flash Rides - Flash speed commuting service.",
+    layout: "guest.hbs"
   });
 });
 
 app.get("/login", function(request, response) {
-  response.render("login", { title: "Login" });
+  response.render("login", { title: "Login", layout: "guest.hbs" });
 });
 
 app.get("/signup", function(request, response) {
-  response.render("signup", { title: "Sign Up" });
+  response.render("signup", { title: "Sign Up", layout: "guest.hbs" });
+});
+
+app.get("/dashboard", function(request, response) {
+  response.render("dashboard", {
+    title: "User Dashboard",
+    page_name: "dashboard",
+    layout: "user.hbs"
+  });
+});
+app.get("/dashboard/rides", function(request, response) {
+  response.render("rides", {
+    title: "User Rides",
+    layout: "user.hbs",
+    page_name: "rides"
+  });
+});
+app.get("/dashboard/billing", function(request, response) {
+  response.render("billing", {
+    title: "User Billing",
+    layout: "user.hbs",
+    page_name: "billing"
+  });
+});
+app.get("/dashboard/profile", function(request, response) {
+  response.render("profile", {
+    title: "User Support",
+    layout: "user.hbs",
+    page_name: "profile"
+  });
+});
+app.get("/dashboard/support", function(request, response) {
+  response.render("support", {
+    title: "User Support",
+    layout: "user.hbs",
+    page_name: "support"
+  });
 });
 
 app.listen(PORT, function() {
