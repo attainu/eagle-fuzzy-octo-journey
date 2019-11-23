92611841085f06b189f4dcaa19c0f13c6911fc5d
sheikh-sadik-vst-au4 <mohdsadik.sadik@gmail.com>
2019-11-23 14:19:43 +0530

    done

--- models/User.js
+++ models/User.js
@@ -1,50 +1,43 @@
-const mongoose = require('mongoose');
+var mongoose = require("mongoose");
 const uniqueValidator = require('mongoose-unique-validator');
+// Get the Schema constructor
+var Schema = mongoose.Schema;
 
+// Using Schema constructor, create a ProductSchema
+var USerSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Can\'t be blank'],
+        trim: true,
+        minlength: [3, 'The length of name should be atleast 3 characters']
+    },
+    phoneno: {
+        type: String,
+        required: [true, 'Can\'t be blank'],
+        trim: true,
+        unique: true,
+        match: [/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/, 'Please fill a valid phone number']
+    },
+    email: {
+        type: String,
+        required: [true, 'Can\'t be blank'],
+        trim: true,
+        lowercase: true,
+        unique: true,
+        uniqueCaseInsensitive: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'Can\'t be blank'],
+        trim: true,
+        minlength: [8, 'Password\'s length should be atleast 8 characters']
+    },
+    rides: [{
+        type: Schema.Types.ObjectId,
+        ref: 'ride'
+    }]
+}, { collection: "user" });
 
-const userSchema = mongoose.Schema({
-
-        name:{
-            type:String,
-            required:[true,'Can\'t be blank'],
-            trim:true,
-            minlength:[3,'The length of name should be atleast 3 characters']
-            
-        },
-        email:{
-            type:String,
-            required:[true,'Can\'t be blank'],
-            trim:true,
-            lowercase:true,
-            unique:true,
-            uniqueCaseInsensitive:true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'Please fill a valid email address']
-
-        },
-        phonenumber:{
-            type:String,
-            required:[true,'Can\'t be blank'],
-            trim:true,
-            unique:true,
-            match:[/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,'Please fill a valid phone number']
-            
-        },
-        password:{
-            type:String,
-            required:[true,'Can\'t be blank'],
-            trim:true,
-            minlength:[8,'Password\'s length should be atleast 8 characters']
-
-        },
-        /*avatar:{
-             type: String,
-             default:''
-        }*/
-        
-    }, { collection:'users'}
-)
-userSchema.plugin(uniqueValidator,{message:'The email or phone number already exists'});
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+// Create model from the schema
+module.exports = mongoose.model("user", USerSchema);
